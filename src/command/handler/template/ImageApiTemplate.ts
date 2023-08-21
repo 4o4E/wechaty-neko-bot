44c3c2f8cb622f5cd88e613d40f1a6d9c49d6b27
404E <869951226@qq.com>
2023-08-21 15:43:45 +0800

    添加模板用于处理通过api获取单个图片并发送的情况

---
+++ src/command/handler/template/ImageApiTemplate.ts
@@ -0,0 +1,48 @@
+import {CommandHandler, CommandHandlerType} from "../CommandHandler";
+import type {Command} from "../../Command";
+import axios, {AxiosRequestConfig, HttpStatusCode} from "axios";
+import {FileBox} from "file-box";
+
+/**
+ * 通过api获取单个图片并发送的指令模板
+ */
+export abstract class SingleImageApiTemplate implements CommandHandler {
+  abstract name: string;
+  abstract regex: RegExp;
+  abstract usage: string;
+  abstract type: CommandHandlerType;
+
+  /**
+   * 构造请求
+   *
+   * @param command 指令
+   */
+  abstract query(command: Command): Promise<{ url: string, config: AxiosRequestConfig } | null>
+
+  onCommand(command: Command): void {
+    this.query(command).then(query => {
+      if (!query) {
+        command.say(this.usage);
+        return null;
+      }
+      let {url, config} = query;
+      return axios.get(url, config);
+    })?.then(resp => {
+      if (resp.status != HttpStatusCode.Ok) {
+        command.say("处理失败");
+        return;
+      }
+      let url = resp.request.res.responseUrl as string;
+      let name = url.substring(url.lastIndexOf("/") + 1);
+      if (name.indexOf(".") == -1) name = `${name}.png`;
+      let file = FileBox.fromBuffer(
+        Buffer.from(resp.data),
+        name
+      );
+      command.say(file).catch(err => {
+        command.say(`发送失败: ${resp.config.url}`);
+        console.error(err);
+      });
+    })
+  }
+}
