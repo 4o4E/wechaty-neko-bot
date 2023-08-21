3b33cb42d5d40de4353feba4927dec07b461b001
404E <869951226@qq.com>
2023-08-21 09:45:19 +0800

    优化代码结构, 修复无效的error日志输出

--- src/main.ts
+++ src/main.ts
@@ -6,19 +6,17 @@ import {MessageManager} from "./MessageManager";
 import {CommandManager} from "./command/manager/CommandManager";
 
 function onScan(qrcode: string, status: ScanStatus) {
-  log.info('Bot', '扫码状态变更: %s(%s)', ScanStatus[status], status)
+  log.info('Bot', '扫码状态变更: %s(%s)', ScanStatus[status], status);
   // 完成扫码后删除二维码文件
   if (status === ScanStatus.Scanned) {
-    fs.unlink("qr.png", (err) => {
-      log.error('bot', err)
-    })
+    fs.unlink("qr.png", err => err && log.error('bot', err))
     return;
   }
   // 生成二维码
   if (status === ScanStatus.Waiting || status === ScanStatus.Timeout) {
-    QRCode.toFile("qr.png", qrcode, {type: 'png'}, (err) => err && log.error('bot', err));
-    log.info('Bot', '等待扫码 - qr.png')
-    log.info('Bot', 'onScan: %s(%s) - %s', ScanStatus[status], status, qrcode)
+    QRCode.toFile("qr.png", qrcode, {type: 'png'}, err => err && log.error('bot', err));
+    log.info('Bot', `等待扫码 - ${process.cwd()}/qr.png`);
+    log.info('Bot', 'onScan: %s(%s) - %s', ScanStatus[status], status, qrcode);
   }
 }
 
@@ -44,5 +42,5 @@ bot.on('scan', onScan)
   .on('logout', onLogout)
   .on('message', onMessage)
   .start()
-  .then(() => log.info('Bot', 'Starter Bot Started.'))
+  .then(() => log.info('Bot', 'Done.'))
   .catch(e => log.error('Bot', e));
