50d4b22d3ba8323710b533567b15cb5924a1c7c4
404E <869951226@qq.com>
2023-08-24 15:16:19 +0800

    移除无效的环境变量

--- run/run.sh
+++ run/run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 rm -rf dist > /dev/null 2>&1
-NEKO_BOT_DIR=run
 cd .. && tsc && cd run && node dist/main.js
--- run/start.sh
+++ run/start.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 rm -rf dist > /dev/null 2>&1
-NEKO_BOT_DIR=run
 
 cd ..
 tsc
